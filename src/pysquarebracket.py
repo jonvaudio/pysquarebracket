39c6e573c99668463115c72a3b03e247e48ce140
Jon V <100223577+jonvaudio@users.noreply.github.com>
2023-10-08 09:56:34 +0100

    basic styles

--- src/pysquarebracket.py
+++ src/pysquarebracket.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import sys
+
 from typing import Union, List
 
 _escape = b'\x1b'
@@ -22,5 +24,18 @@ def _format(styles: Union[str, List[str]]) -> bytes:
     styles = [styles]
   return b'\x1b[' + b';'.join(map(lambda style: _formats[style], styles)) + b'm'
 
+def set_format(styles: Union[str, List[str]]) -> None:
+  sys.stdout.buffer.write(_format(styles))
+  sys.stdout.buffer.flush()
+
 if __name__ == '__main__':
-  print(_format('not_bright'))
+  for style1 in _formats.keys():
+    if not style1.startswith('not_') and style1 != 'plain':
+      set_format(style1)
+      print(style1)
+      set_format('plain')
+      for style2 in _formats.keys():
+        if style2 != style1 and not style2.startswith('not_') and style2 != 'plain':
+          set_format([style1, style2])
+          print(f'{style1} and {style2}')
+          set_format('plain')
